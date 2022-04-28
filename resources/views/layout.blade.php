f93d89df0306af901a797619bf67ffd5c34e7b70
Gabriel <gabriel@sysbase.com.br>
2022-04-28 14:57:36 -0300

    Adicionado css personalizado

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,6 +6,9 @@
         <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
         <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png">
 
+        <!-- Estilo personalizado -->
+        <link rel="stylesheet" href="/css/estilo.css">
+        
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
